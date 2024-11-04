3b6d5e03951b26c64c02c137c54b6e62b07925e7
Thomas Rancourt-Cote <2142381@csfoy.ca>
2024-11-04 09:50:37 -0500

    début implémentation du Wizard

--- Assets/Scripts/Wizard/Wizard.cs
+++ Assets/Scripts/Wizard/Wizard.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class Wizard : MonoBehaviour
 {
-    //Enum pour séparer team 1 de team 2
+    [SerializeField] private GameObject HealthBar;
+    private Teams curentTeam;
     
     void Awake()
     {
-        
+
+    }
+
+    public void SetTeam(Teams team)
+    {
+        curentTeam = team;
     }
 
     void Update()
     {
+        gameObject.transform.position += new Vector3(0.01f, 0.01f);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         
     }
 }
+
+public enum Teams { TEAM1, TEAM2 };
