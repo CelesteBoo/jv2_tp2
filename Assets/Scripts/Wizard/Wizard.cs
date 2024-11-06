5acb1e52b69b3d923793a9f75e99e12b54569432
Thomas Rancourt-Cote <2142381@csfoy.ca>
2024-11-06 11:49:55 -0500

    Implementation of range for wizard

--- Assets/Scripts/Wizard/Wizard.cs
+++ Assets/Scripts/Wizard/Wizard.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 public class Wizard : MonoBehaviour
 {
     [SerializeField] private GameObject HealthBar;
+    [SerializeField] private GameObject range;
+    [SerializeField] private int health = 100;
+    [SerializeField] private float speed = 0.1f;
+    [SerializeField] private Teams team;
+
+
     private Teams curentTeam;
+    private WizardBlackboard blackBoard;
+    private Wizard activeTarget;
+
+    public Teams CurrentTeam => team;
+    public int CurrentHP => health;
     
     void Awake()
     {
-
+        blackBoard = new WizardBlackboard(new WizardNormalState(), new WizardIntrepidState(), new WizardFleeState(), new WizardHideState(), new WizardSafeState());
     }
 
     public void SetTeam(Teams team)
@@ -15,15 +26,41 @@ public class Wizard : MonoBehaviour
         curentTeam = team;
     }
 
+    public void SetBaseTarget(GameObject tower)
+    {
+
+    }
+
     void Update()
     {
-        gameObject.transform.position += new Vector3(0.01f, 0.01f);
+        if (activeTarget != null)
+        {
+            //shoot target
+            if (activeTarget.CurrentHP <= 0)
+            {
+                activeTarget = null;
+            }
+            
+            return;
+        }
+        
+        gameObject.transform.position += new Vector3(speed, speed);
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void WizardToShoot(Wizard wizard)
     {
-        
+        if (activeTarget == null && wizard.CurrentTeam != team)
+        {
+            activeTarget = wizard;
+        }
+        Debug.Log(wizard.CurrentTeam);
+        Debug.Log(team);
+    }
+
+    public void TowerToShoot(GameObject tower)
+    {
+
     }
 }
 
